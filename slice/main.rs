8e0f62cddf6e7060106e4a242c9828ce2c01aef8
wangrenquan <1782317677@qq.com>
2022-09-30 00:24:22 +0800

    testing rust install

--- slice/main.rs
+++ slice/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    
+    let my_sring = String :: from ("Hello world");
+    let wordIndex = first_word(&my_sring[..]);
+    println!("words:{}",wordIndex);
+    let my_sring_literal = "Hello world";
+    let wordIndex = first_word(my_sring_literal);
+    println!("words:{}",wordIndex);
+}
+
+fn first_word(s: &str)->&str{
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate(){
+        if item ==b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
 }
