1f467897107ec97dc5a3525adbeaca74521c60c8
wangrenquan <1782317677@qq.com>
2022-09-30 01:23:32 +0800

    finished calculate_area

---
+++ calculate_area/src/main.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    length: u32,
+}
+
+fn main() {
+    let rect = Rectangle { width: 30, length: 40 };
+    println!("{}", area(&rect));
+    println!("{:#?}", rect);
+}
+
+fn area(rect: &Rectangle) -> u32 {
+    rect.width * rect.length
+}
