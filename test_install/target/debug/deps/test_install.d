daedb010679dee30b8ae52181fd71b5451441a0c
wangrenquan <1782317677@qq.com>
2022-09-30 00:28:16 +0800

    testing rust install

---
+++ test_install/target/debug/deps/test_install.d
@@ -0,0 +1,5 @@
+D:\BaiduSyncdisk\learn_rust\test_install\target\debug\deps\test_install.exe: src\main.rs
+
+D:\BaiduSyncdisk\learn_rust\test_install\target\debug\deps\test_install.d: src\main.rs
+
+src\main.rs:
---
+++ test_install/target/debug/test_install.d
@@ -0,0 +1,1 @@
+D:\BaiduSyncdisk\learn_rust\test_install\target\debug\test_install.exe: D:\BaiduSyncdisk\learn_rust\test_install\src\main.rs
